d7a9bae7199c3ac2456a6c9ea44b9160271ef492
etorreborre <etorreborre@yahoo.com>
2011-03-05 17:40:25 +1100

    reset the build props

--- project/build/project.scala
+++ project/build/project.scala
@@ -17,21 +17,21 @@ class Project(info: ProjectInfo) extends DefaultWebProject(info) with ScctProjec
 										  s.endsWith("Suite") || 
 										  s.endsWith("Unit") }
 
-  val specs2 		= "org.scala-tools.testing" %% "specs2" % "1.0-SNAPSHOT" 
-  val scalacheck 		= "org.scala-tools.testing" %% "scalacheck" % "1.8" 
-  val testinterface 	= "org.scala-tools.testing" % "test-interface" % "0.5" 
-  val scalazcore 		= "com.googlecode.scalaz" %% "scalaz-core" % "5.1-SNAPSHOT"
-  val hamcrest             = "org.hamcrest" % "hamcrest-all" % "1.1"
+  val specs2 		= "org.specs2" %% "specs2" % "1.0-SNAPSHOT" 
+  val scalacheck 	= "org.scala-tools.testing" %% "scalacheck" % "1.8" 
+  val testinterface = "org.scala-tools.testing" % "test-interface" % "0.5" 
+  val scalazcore 	= "com.googlecode.scalaz" %% "scalaz-core" % "5.1-SNAPSHOT"
+  val hamcrest      = "org.hamcrest" % "hamcrest-all" % "1.1"
   val mockito 	 	= "org.mockito" % "mockito-all" % "1.8.5" 
-  val junit 			= "junit" % "junit" % "4.7"
-  val parboiled             =  "org.parboiled" % "parboiled4j" % "0.9.9.0"
-  val pegdown             =  "org.pegdown" % "pegdown" % "0.8.5.4"
+  val junit 		= "junit" % "junit" % "4.7"
+  val parboiled     =  "org.parboiled" % "parboiled4j" % "0.9.9.0"
+  val pegdown       =  "org.pegdown" % "pegdown" % "0.8.5.4"
 
-  val webkit                =  "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default"
-  val mapper               =  "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default"
-  val h2                     =  "com.h2database" % "h2" % "1.2.138"
-  val jfreechart      =  "jfree" % "jfreechart" % "1.0.11"
-  val jfreecommon      =  "jfree" % "jcommon" % "1.0.14"
+  val webkit        =  "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default"
+  val mapper        =  "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default"
+  val h2            =  "com.h2database" % "h2" % "1.2.138"
+  val jfreechart    =  "jfree" % "jfreechart" % "1.0.11"
+  val jfreecommon   =  "jfree" % "jcommon" % "1.0.14"
   
   override val jettyPort = 8090
   
